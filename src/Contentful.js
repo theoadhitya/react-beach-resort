7602787139dc6020c1d1e47c1e97e72999bea3c8
theoadhitya <46550660+theoadhitya@users.noreply.github.com>
2019-05-11 10:48:25 +0700

    Update Contentful.js

--- src/Contentful.js
+++ src/Contentful.js
@@ -3,6 +3,4 @@ import { createClient } from "contentful";
 export default createClient({
   space: process.env.REACT_APP_API_SPACE,
   accessToken: process.env.REACT_APP_ACCESS_TOKEN
-  // space: "bv6r72xg4tda",
-  // accessToken: "CESp6Zmp5d1mcLK6ptyQSiR8MkBYNTrSwxbIJlS9ZCQ"
 });
